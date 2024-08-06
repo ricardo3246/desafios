998c7ab1d7dc594f3b8b2a2289bd65ae85696ad5
ricardo3246 <159441570+ricardo3246@users.noreply.github.com>
2024-08-06 08:08:54 -0300

    Add files via upload

---
+++ desafio12js.js
@@ -0,0 +1,27 @@
+// Get the element with id 'saida'
+var jogahtml = window.document.getElementById('saida');
+
+// Function for button 1
+function clique1() {
+    jogahtml.innerHTML += '<p>Você clicou e escolheu a primeira opção - Botão 1</p>';
+}
+
+// Function for button 2
+function clique2() {
+    jogahtml.innerHTML += '<p>Você clicou e escolheu a segunda opção - Botão 2</p>';
+}
+
+// Function for button 3
+function clique3() {
+    jogahtml.innerHTML += '<p>Você clicou e escolheu a terceira opção - Botão 3</p>';
+}
+
+
+// Function for button 4
+function clique4() {
+    jogahtml.innerHTML += '<p>Você clicou e escolheu a quarta opção - Botão 4</p>';
+}
+
+
+
+
